008295e393947ef9b508419348e03cbf99ba648d
edopaz798 <edopaz798@users.noreply.github.com>
2016-11-08 22:18:14 -0500

    Update example.js

--- example.js
+++ example.js
@@ -4,21 +4,24 @@ var app = express()
 var request = require("request");
 var cheerio = require('cheerio');
 
-var nrogui = 'VAL0391100661';
+/*var nrogui = 'VAL0391100661';*/
+
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-	
-	var options = { method: 'POST',
+app.post('/', urlencodedParser, function (req, res) {
+
+  var nrogui = req.body.nrogui;	
+  var options = { method: 'POST',
   url: 'http://arexpress.controlbox.net/app/rastreo/rastreo.asp',
   headers: 
    { 'cache-control': 'no-cache',
      'content-type': 'application/x-www-form-urlencoded' },
   form: { nrogui: nrogui, ffw: '00001' } };
-  
-  
+	
   request(options, function (error, response, body) {
 	  if(!error){
 		  var $ = cheerio.load(body.replace(/..\/images\/Cientes\/arExpress.jpg/g, "http://www.emcabox.com/rastreo/emcabox.jpg"));
@@ -27,8 +30,10 @@ app.get('/', function (req, res) {
 		  res.send(body2)
 	  }
   
+  })
 })
-})
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
